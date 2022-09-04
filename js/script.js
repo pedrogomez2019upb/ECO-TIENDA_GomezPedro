ead3129f0b5da7003e9e51a9c68549225a577d6f
Pedro Felipe Gómez Bonilla <pedro.gomez.2019@upb.edu.co>
2022-09-04 00:18:12 -0500

    Trying Discount

--- js/script.js
+++ js/script.js
@@ -6,21 +6,58 @@ function call(){
 call();
 
 function productoF(item,index){
-    const panel=
-    `
-                <div class="col-sm-3 producto" id="card" >
-                <figure>
-                    <img src="${item.img}" alt="figura1" >
-                </figure>
-                <div class="contenido">
-                    <h6>${item.title}</h6>
-                    <h6>${item.size}</h6>
-                    <h7 class="precio">${item.price}</h7>
-                </div>
-            </div>
+    if(item.discount!=""){
+        const card = 
         `
-        console.log(index);
-        document.getElementById("row").innerHTML += panel;
+            <div class="col-sm-1 producto" id="card" >
+                            <div class="discount">
+                                <h1>${item.discount}</h1>
+                            </div>
+                            <div class="favorito">
+                            <figure>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                </svg>
+                            </figure>
+                            </div>
+                            <figure>
+                                <img src="${item.img}" alt="figura1" >
+                            </figure>
+                            <div class="contenido">
+                                <h6>${item.title}</h6>
+                                <h6>${item.size}</h6>
+                                <h7 class="precio">${item.price}</h7>
+                            </div>
+                            <button class="boton">Añadir a la cesta</button>
+                            
+                        </div>
+            
+            
+            `
+            console.log(index);
+            document.getElementById("row").innerHTML+=card;
+    }
+    else{
+        const panel =
+        `
+        <div class="col-sm-3 producto" id="card" >
+        <figure>
+            <img src="${item.img}" alt="figura1" >
+        </figure>
+        <div class="contenido">
+            <h6>${item.title}</h6>
+            <h6>${item.size}</h6>
+            <h7 class="precio">${item.price}</h7>
+        </div>
+    </div>
+            `
+
+            console.log(index);
+            document.getElementById("row").innerHTML+=panel;
+    
+
+    }
+
 }
 
 
